901d5258207b4cf93dbe40ac2300d5990d257779
AndyNotfound <Andy.Notfound@gmail.com>
2023-01-06 11:06:37 -0500

    Add: API call from the movie db

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: "24939fe05142819ad6a2cc90c6cbc671",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
