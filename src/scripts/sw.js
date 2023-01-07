2f66e7f50e396c6db34aaa5a074bc79a7629538e
AndyNotfound <Andy.Notfound@gmail.com>
2023-01-07 10:07:53 -0500

    Add: Finishing cache and service worker module, next I'll use workbox

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,15 +1,35 @@
+import "regenerator-runtime";
+import cacheHelper from "./utils/cache-helper";
+
+const assetsToCache = [
+  "./",
+  "./icons/maskable_icon.png",
+  "./icons/maskable_icon_x48.png",
+  "./icons/maskable_icon_x72.png",
+  "./icons/maskable_icon_x96.png",
+  "./icons/maskable_icon_x128.png",
+  "./icons/maskable_icon_x192.png",
+  "./icons/maskable_icon_x384.png",
+  "./icons/maskable_icon_x512.png",
+  "./index.html",
+  "./favicon.png",
+  "./app.bundle.js",
+  "./app.webmanifest",
+  "./sw.bundle.js",
+];
+
 self.addEventListener("install", (e) => {
   console.log("Installing service worker ...");
-  //   TODO: Caching App Shell Resource
+  cacheHelper.cachingAppShell([...assetsToCache]);
 });
 
 self.addEventListener("activate", (e) => {
   console.log("Activating service worker ...");
-  // TODO: Deleting Old Caches
+  cacheHelper.deleteOldCache();
 });
 
 self.addEventListener("fetch", (e) => {
   console.log(e.request);
   e.respondWith(fetch(e.request));
-  //   TODO: HTTP Request to/from Cache
+  cacheHelper.revalidateCache(e.request);
 });
